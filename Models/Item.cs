261fb806805f99a18272ea563a1e6078217c8e1f
Minh Tran Duc <minh.td194331@sis.hust.edu.vn>
2023-01-26 22:38:08 +0700

    update file index frontend

--- Models/Item.cs
+++ Models/Item.cs
@@ -17,16 +17,20 @@ namespace OfficialProject3.Models
     }
     public class Item
     {
-        private static int StaticID = 1;
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage="Hãy nhập tên file!")]
+        [Display(Name = "Tên tài liệu")]
         public string Name { get; set; } = String.Empty;
+        [Display(Name = "Mô tả")]
         public string Description { get; set; } = String.Empty;
+        [Display(Name = "Phân loại")]
         public FileType Type { get; set; }
         [NotMapped]
+        [Display(Name = "Số lượt xem")]
         public int ViewedCount { get; } = 0;
         [NotMapped]
+        [Display(Name = "Số lượt tải về")]
         public int DownloadedCount { get; } = 0;
         [Display(Name = "Mã môn học")]
         [ForeignKey("Mã môn học")]
@@ -36,27 +40,21 @@ namespace OfficialProject3.Models
         [ForeignKey("User")]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string UserId { get; set; }
+        [Display(Name = "Tác giả")]
         //Navigation props
         public User User { get; set; }
         public Item() {}
         public Item(string name, string description, FileType type,
             string subjectCode, string fileLink, string userId) 
         {
-            Id = ++StaticID;
+            Random rand = new Random();
+            Id = rand.Next(100000000);
             Name = name;
             Description = description;
             Type = type;
             SubjectCode = subjectCode;
             FileLink = fileLink;
             UserId = userId;
-        }
-        public Item(string name, string description, FileType type, string subjectCode)
-        {
-            Id = StaticID++;
-            Name = name;
-            Description = description;
-            Type = type;
-            SubjectCode = subjectCode;
         }
     }
 }
